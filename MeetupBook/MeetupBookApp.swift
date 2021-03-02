d9819dc7d52f0a8bb1b25fb2105ffb5dee6916ee
Scott Obara <scott@rubbscreations.com>
2021-03-02 20:35:01 +1100

    Initial Commit

---
+++ MeetupBook/MeetupBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupBookApp.swift
+//  MeetupBook
+//
+//  Created by Scott Obara on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MeetupBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
